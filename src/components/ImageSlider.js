56c94952646811e41ffd989f4f0af1561778b89d
Reiker <reiker1@hotmail.com>
2023-02-02 23:01:41 +0100

    urlBack

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -33,7 +33,7 @@ useEffect(() => {
    <img onError={({ currentTarget }) => {
             currentTarget.onerror = null; // prevents looping
             currentTarget.src = "https://img.freepik.com/vector-gratis/error-404-ilustracion-concepto-paisaje_114360-7888.jpg";
-          }} src={!imagenArray.length ? "https://img.huffingtonpost.es/files/main_image_720_480/uploads/2023/01/11/63bef9cfed281.jpeg" : imagenArray.hits[current].largeImageURL} alt='travel image' className='image' /> 
+          }} src={imagenArray.hits[current].largeImageURL} alt='travel image' className='image' /> 
    
    {   <button className="left-arrow" onClick={()=>current === 0 ? "":setCurrent(current - 1) } > - </button>}
   </div>
