ea633dd1903ee23ce7b3fa03ade08e45ce012a66
Reiker <reiker1@hotmail.com>
2023-02-04 18:28:28 +0100

    urlError

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import UserConsumer from "../hooks/useDatos";
+import Spain from "../images/spain.jpeg"
 
 const ImageSlider = () => {
 
@@ -12,7 +13,7 @@ const ImageSlider = () => {
 useEffect(() => {
 
   
-  fetch(`https://pixabay.com/api/?key=32822302-e0bccee14336fc56618e7f358&q=${saveResultFinal[1]}+city&image_type=photo&pretty=true`)
+  fetch(`https://pixabay.com/api/?key=32822302-e0bccee14336fc56618e7f358&q=${saveResultFinal[2]}+city&image_type=photo&pretty=true`)
   .then(response => response.json())
   .then(data =>   setImagenArray(data));
 
@@ -33,7 +34,7 @@ useEffect(() => {
    <img onError={({ currentTarget }) => {
             currentTarget.onerror = null; // prevents looping
             currentTarget.src = "https://img.freepik.com/vector-gratis/error-404-ilustracion-concepto-paisaje_114360-7888.jpg";
-          }} src={imagenArray.hits[current].largeImageURL} alt='travel image' className='image' /> 
+          }} src={imagenArray.total === 0 ? Spain : imagenArray.hits[current].largeImageURL} alt='travel image' className='image' /> 
    
    {   <button className="left-arrow" onClick={()=>current === 0 ? "":setCurrent(current - 1) } > - </button>}
   </div>
